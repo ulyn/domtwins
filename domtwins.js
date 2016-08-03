d2077a55318fe24ffab63f48818531a040cecfd5
ulyn <ulyn@jmuca.com>
2016-08-03 21:11:44 +0800

    Initial commit

---
+++ domtwins.js
@@ -0,0 +1,122 @@
+"use strict";
+(function (factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory(require('jQuery'));
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jQuery'],factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    if(!$){
+        throw new Error("缺少依赖的jQuery");
+    }
+    var DOM_TWINS_ID = "dom-twins-id";
+    var cache = {};
+
+    function open(url,onclose){
+        this.onclose = onclose;
+        var selector = this.selector;
+        selector.hide();
+        this.htmlLoaderDom.hide();
+        $("iframe",this.iframeDom).attr("src",url);
+        this.iframeDom.show();
+    };
+
+    function openHtml(html,onclose){
+        this.onclose = onclose;
+        var selector = this.selector;
+        selector.hide();
+        this.iframeDom.hide();
+        this.htmlLoaderDom.show();
+    };
+
+    function close(){
+        var selector = this.selector;
+        selector.show();
+        this.iframeDom.hide();
+        this.htmlLoaderDom.hide();
+        this.onclose && this.onclose(this);
+        delete this.onclose;
+    };
+
+    function DomTwins(selector){
+        this.selector = selector;
+        //初始化;
+        this.htmlLoaderDom = selector.clone(true).attr("copyNode",1).empty().hide();
+        this.iframeDom = this.htmlLoaderDom.clone(true).html("<iframe src='' frameborder='0' width='100%' height='100%'></iframe>");
+        $(selector).after(this.htmlLoaderDom).after(this.iframeDom);
+
+        var domTwinsId = this.selector.attr(DOM_TWINS_ID);
+        if(!domTwinsId){
+            domTwinsId = "id_" + new Date().getTime() + Math.random();
+            this.selector.attr(DOM_TWINS_ID,domTwinsId);
+        }
+        cache[domTwinsId] = this;
+    }
+
+    DomTwins.prototype = {
+        open:open,
+        close:close
+    }
+
+    DomTwins.parentClose = function(obj){
+        var bodyrel="temp"+ new Date().getTime();
+        var curDocument = obj.ownerDocument || obj.document;
+        $("body",curDocument).attr("dom-twins-body-rel",bodyrel);
+        var parentDocument = curDocument.defaultView.parent.document;
+        $(parentDocument).find("iframe").each(function(){
+            var iframe = $(this);
+            var tRel=$(this.contentWindow.document).find("body").attr("dom-twins-body-rel");
+            if(tRel==bodyrel){
+                var $target = iframe.parent().prev();
+                var domTwins = cache[$target.attr(DOM_TWINS_ID)];
+                domTwins.close();
+            }
+        });
+    }
+
+    $.fn.DomTwins = function(selector){
+        if(!selector){
+            selector = $(this);
+        }
+        var domTwinsId = $(selector).attr(DOM_TWINS_ID);
+        if(domTwinsId){
+            return cache[domTwinsId] || new DomTwins(selector);
+        }else{
+            return new DomTwins(selector);
+        }
+    }
+    //支持配置的方式的节点
+//    dom-twins = target
+    $(document).on("click","[dom-twins-target]",function(){
+        var $this = $(this);
+        var targetId = $this.attr("dom-twins-target");
+        var $target = $("["+ DOM_TWINS_ID +"="+ targetId +"]");
+        var domTwins = cache[targetId];
+        if(!domTwins){
+            domTwins = new DomTwins($target);
+        }
+        domTwins.open( $this.attr("dom-twins-href"),
+            (function(){
+                var onclose = $this.attr("dom-twins-onclose");
+                if(onclose){
+                    var tempFunc = eval(onclose);
+                    return tempFunc;
+                }
+            })()
+        );
+        return false;
+    });
+
+    window.DomTwins = DomTwins;
+    return DomTwins;
+
+}));
+
+
